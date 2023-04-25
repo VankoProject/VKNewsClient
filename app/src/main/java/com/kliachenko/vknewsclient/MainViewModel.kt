37ecdc5894ed78ddc9e23d1b2c0b59c128e43042
VankoProject <ivan.green69@gmail.com>
2023-04-25 19:33:38 +0300

    add ViewModel and delegate update function by liveData

---
+++ app/src/main/java/com/kliachenko/vknewsclient/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.kliachenko.vknewsclient
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.kliachenko.domain.FeedPost
+import com.kliachenko.domain.StatisticItem
+
+class MainViewModel : ViewModel() {
+
+    private val _feedPost = MutableLiveData(FeedPost())
+    val feedPost: LiveData<FeedPost> = _feedPost
+
+    fun updateCount(item: StatisticItem) {
+        val oldStatistics = feedPost.value?.statistics ?: throw java.lang.IllegalStateException()
+        val newStatistics = oldStatistics.toMutableList().apply {
+            replaceAll { oldItem ->
+                if (oldItem.type == item.type) {
+                    oldItem.copy(count = oldItem.count + 1)
+                } else {
+                    oldItem
+                }
+            }
+        }
+        _feedPost.value = feedPost.value?.copy(statistics = newStatistics)
+    }
+
+}
