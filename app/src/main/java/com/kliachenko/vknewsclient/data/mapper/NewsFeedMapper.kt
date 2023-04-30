286561b236ae09eb0404afa1dda03ed30c3c3e2c
VankoProject <ivan.green69@gmail.com>
2023-04-30 19:36:08 +0300

    format displaying counts of statistics (likes, comments, shares, views) set two types of favourite icon depends on post's status

--- app/src/main/java/com/kliachenko/vknewsclient/data/mapper/NewsFeedMapper.kt
+++ app/src/main/java/com/kliachenko/vknewsclient/data/mapper/NewsFeedMapper.kt
@@ -4,6 +4,8 @@ import com.kliachenko.vknewsclient.data.model.NewsFeedResponseDto
 import com.kliachenko.vknewsclient.domain.FeedPost
 import com.kliachenko.vknewsclient.domain.StatisticItem
 import com.kliachenko.vknewsclient.domain.StatisticType
+import java.text.SimpleDateFormat
+import java.util.*
 import kotlin.math.absoluteValue
 
 class NewsFeedMapper {
@@ -19,7 +21,7 @@ class NewsFeedMapper {
             val feedPost = FeedPost(
                 id = post.id,
                 communityName = group.name,
-                publicationDate = post.date.toString(),
+                publicationDate = mapTimestampToDate(post.date * 1000),
                 communityImageUrl = group.imageUrl,
                 contentText = post.text,
                 contentImageUrl = post.attachments?.firstOrNull()?.photo?.photoUrls?.lastOrNull()?.url,
@@ -28,11 +30,16 @@ class NewsFeedMapper {
                     StatisticItem(type = StatisticType.VIEWS, post.views.count),
                     StatisticItem(type = StatisticType.SHARES, post.reposts.count),
                     StatisticItem(type = StatisticType.COMMENTS, post.comments.count),
-                )
+                ),
+                isFavourite = post.isFavourite
             )
             result.add(feedPost)
         }
-
         return result
+    }
+
+    private fun mapTimestampToDate(timestamp: Long): String {
+        val date = Date(timestamp)
+        return SimpleDateFormat("d MMMM yyyy, hh:mm", Locale.getDefault()).format(date)
     }
 }
